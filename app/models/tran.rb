0bec1f1863e8c873566a5721351471b10d65a199
Rick Wang <lurker2000wxc@gmail.com>
2018-09-30 08:10:49 +1000

    allow optional

--- app/models/tran.rb
+++ app/models/tran.rb
@@ -3,7 +3,7 @@ class Tran < ApplicationRecord
 	CREDIT = 'credit'
 	DEBIT = 'debit'
 	TRANSFER = 'transfer'
-	belongs_to :source_entity, polymorphic: true
-	belongs_to :target_entity, polymorphic: true
+	belongs_to :source_entity, polymorphic: true, optional: true
+	belongs_to :target_entity, polymorphic: true, optional: true
 
 end
