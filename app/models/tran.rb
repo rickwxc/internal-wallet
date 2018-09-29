1a9d881cfc914df8035472085ff80b0056d684f0
Rick Wang <lurker2000wxc@gmail.com>
2018-09-29 22:54:08 +1000

    interface

--- app/models/tran.rb
+++ app/models/tran.rb
@@ -1,4 +1,9 @@
 class Tran < ApplicationRecord
-  belongs_to :source_entity, polymorphic: true
-  belongs_to :target_entity, polymorphic: true
+
+	CREDIT = 'credit'
+	DEBIT = 'debit'
+	TRANSFER = 'transfer'
+	belongs_to :source_entity, polymorphic: true
+	belongs_to :target_entity, polymorphic: true
+
 end
